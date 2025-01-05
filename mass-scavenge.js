ef54b89523e4195969eca1a0eac9095e51eaeafc
UHH18 <vomanderenplaneten@t-online.de>
2025-01-05 22:10:20 +0100

    Update mass-scavenge.js

--- mass-scavenge.js
+++ mass-scavenge.js
@@ -1,4 +1,4 @@
-
+javascript:
 //mass scavenging by Sophie "Shinko to Kuma"
 serverTimeTemp = $("#serverDate")[0].innerText + " " + $("#serverTime")[0].innerText;
 serverTime = serverTimeTemp.match(/^([0][1-9]|[12][0-9]|3[01])[\/\-]([0][1-9]|1[012])[\/\-](\d{4})( (0?[0-9]|[1][0-9]|[2][0-3])[:]([0-5][0-9])([:]([0-5][0-9]))?)?$/);
